226f9de9e083aca2c9fc184ea2a7049354f09951
Manny2023 <120243119+Manny2023@users.noreply.github.com>
2024-10-16 11:38:35 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,12 @@
-window.OneSignalDeferred.push(async function(OneSignal) {
-    await OneSignal.init({
-        appId: "d7cce0be-3363-4a4f-b359-052bbbe8e096"
-    });
-
-    OneSignal.push(() => {
-        OneSignal.isPushNotificationsEnabled().then(function(isEnabled) {
-            if (isEnabled) {
-                console.log("Push notifications are enabled!");
-            } else {
-                console.log("Push notifications are not enabled!");
-                OneSignal.push(() => {
-                    OneSignal.showNativePrompt(); // Muestra el prompt de notificaciones
-                });
-            }
-        });
-    });
+self.addEventListener('push', function(event) {
+    const data = event.data ? event.data.json() : {};
+    const options = {
+        body: data.body,
+        icon: data.icon,
+        badge: data.badge
+    };
+    
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
 });
